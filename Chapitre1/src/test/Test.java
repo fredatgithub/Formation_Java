ba93235c52b078c17334530b12d28701591db1a1
Fred <domaine75@gmail.com>
2018-09-10 13:17:29 +0200

    Initial commit

---
+++ Chapitre1/src/test/Test.java
@@ -0,0 +1,12 @@
+package test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		System.out.println("hello formation Java");
+		System.out.println("sysout CTRL SPace pour avoir un System.out.println()");
+		System.out.println("CTRL F11 to run");
+
+	}
+
+}
